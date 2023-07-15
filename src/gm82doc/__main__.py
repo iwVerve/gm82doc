f7b51280052a2b317d042bb0b59f72a6e74ff0d0
iwVerve <thedolphiner@gmail.com>
2023-07-15 20:05:51 +0200

    read script doc

--- src/gm82doc/__main__.py
+++ src/gm82doc/__main__.py
@@ -6,13 +6,14 @@ from pathlib import Path
 
 
 @define
-class ScriptGroup:
+class ScriptFolder:
     name: str
-    children: list[Union["ScriptGroup", str]] = Factory(list)
+    children: list[Union["ScriptFolder", str]] = Factory(list)
 
 
 @define
 class Script:
+    signature: str = ""
     short_doc: str = ""
     long_doc: str = ""
 
@@ -23,27 +24,30 @@ class Script:
 
 @define
 class GM82Project:
+    path: Path
     scripts: dict[str, Script]
-    script_tree: ScriptGroup
+    script_tree: ScriptFolder
 
     def __init__(self, project_path: str):
+        self.path = project_path
         self.scripts = {}
-        self.script_tree = ScriptGroup("Scripts")
-        self.open(project_path)
+        self.script_tree = ScriptFolder("Scripts")
+        self.open()
 
-    def open(self, project_path_str: str):
-        if not project_path_str.endswith("\\"):
-            project_path_str += "\\"
-        project_path = Path(project_path_str)
+    def open(self):
+        if not self.path.endswith("\\"):
+            self.path += "\\"
+        self.path = Path(self.path)
 
-        script_folder = project_path.joinpath(r"scripts\\")
-        script_tree_path = script_folder.joinpath(r"tree.yyd")
+        self.read_script_tree()
+        self.read_script_docs()
+        print(self.scripts["example_script"])
 
-        script_tree = ScriptGroup("Scripts")
+    def read_script_tree(self):
         current_path = []
-        current_folder = script_tree
+        current_folder = self.script_tree
         current_depth = 0
-        with script_tree_path.open() as script_tree_file:
+        with self.get_script_tree().open() as script_tree_file:
             for line in script_tree_file.readlines():
                 depth = len(line)
                 line = line.lstrip("\t")
@@ -55,7 +59,7 @@ class GM82Project:
 
                 if line.startswith("+"):
                     line = line[1:-1]
-                    new_folder = ScriptGroup(line)
+                    new_folder = ScriptFolder(line)
                     current_folder.children.append(new_folder)
                     current_path.append(current_folder)
                     current_folder = new_folder
@@ -65,6 +69,57 @@ class GM82Project:
                     new_script = Script(line)
                     self.scripts[line] = new_script
                     current_folder.children.append(line)
+
+    def read_script_docs(self):
+        for script_name, script in self.scripts.items():
+            script_path = self.get_script_folder().joinpath(f"{script_name}.gml")
+            with script_path.open() as script_file:
+                state = 0
+                can_read_signature = True
+                can_read_short_doc = True
+                can_read_long_doc = True
+                first_long_doc_line = True
+                for line in script_file.readlines():
+                    line = line[:-1]
+
+                    if state == 1:
+                        if line.startswith("//"):
+                            script.short_doc += " " + line[2:].strip()
+                        else:
+                            state = 0
+                    elif state == 2:
+                        if line == "*/":
+                            state = 0
+                            continue
+                        else:
+                            if first_long_doc_line:
+                                first_long_doc_line = False
+                            else:
+                                script.long_doc += " "
+                            script.long_doc += line.strip()
+
+                    if state == 0:
+                        if can_read_signature and line.startswith("///"):
+                            script.signature = line[3 + len(script_name) :].strip()
+                        elif can_read_short_doc and line.startswith("//"):
+                            script.short_doc += line[2:].strip()
+                            can_read_short_doc = False
+                            state = 1
+                        elif can_read_long_doc and line.startswith("/*doc"):
+                            can_read_long_doc = False
+                            state = 2
+                        elif line.strip() != "":
+                            break
+
+                    can_read_signature = False
+                    if state == 0 and not can_read_short_doc and not can_read_long_doc:
+                        break
+
+    def get_script_folder(self) -> Path:
+        return self.path.joinpath(r"scripts\\")
+
+    def get_script_tree(self) -> Path:
+        return self.get_script_folder().joinpath(r"tree.yyd")
 
 
 if __name__ == "__main__":
